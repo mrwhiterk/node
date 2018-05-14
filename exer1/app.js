4051ca3629bb68f9f1e7bc0faaa5f47a93f3ba6b
Ryan White <ryanwhite@Ryans-iMac.lan>
2018-05-13 21:54:29 -0400

    printing out js points now

--- exer1/app.js
+++ exer1/app.js
@@ -6,25 +6,25 @@ const username = "ryanwhite89";
 
 //function to print message to console
 function printMessage(username, badgeCount, points) {
-    const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
-    console.log(message);
+    const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`
+    console.log(message)
 }
 
 
 // connect to api url (https://teamtreehouse.com/ryanwhite89.json)
 const request = https.get(`https://teamtreehouse.com/${username}.json`, res => {
-    let body = "";
+    let body = ""
     // read the data
     res.on('data', data => {
-        body += data.toString();
-    });
+        body += data.toString()
+    })
 
     res.on('end', () => {
         // parse the data
-        console.log(body);
-        console.log(typeof body);
+        const profile = JSON.parse(body)
         // print the data
-    });
+        printMessage(username, profile.badges.length, profile.points.JavaScript)
+    })
 
-});
+})
 
