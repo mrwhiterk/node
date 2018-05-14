a3b175255216d2427231bc23b469630b572ddd9b
Ryan White <ryanwhite@Ryans-iMac.lan>
2018-05-13 22:37:37 -0400

    added error handling for https get

--- exer1/app.js
+++ exer1/app.js
@@ -6,29 +6,35 @@ const https = require('https');
 //function to print message to console
 function printMessage(username, badgeCount, points) {
     const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`
-    console.log(message)
+    console.log(message);
 }
 
 function getProfile(username) {
-    // connect to api url (https://teamtreehouse.com/ryanwhite89.json)
-    const request = https.get(`https://teamtreehouse.com/${username}.json`, res => {
-        let body = ""
-        // read the data
-        res.on('data', data => {
-            body += data.toString()
+    try {
+        // connect to api url (https://teamtreehouse.com/ryanwhite89.json)
+        const request = https.get(`https://teamtreehouse.com/${username}.json`, (res)  => {
+            let body = ""
+            // read the data
+            res.on('data', data => {
+                body += data.toString();
+            })
+        
+            res.on('end', () => {
+                // parse the data
+                const profile = JSON.parse(body);
+                // print the data
+                printMessage(username, profile.badges.length, profile.points.JavaScript);
+            })
+            
         })
-    
-        res.on('end', () => {
-            // parse the data
-            const profile = JSON.parse(body)
-            // print the data
-            printMessage(username, profile.badges.length, profile.points.JavaScript)
-        })
-    
-    })
+
+        request.on('error', err => console.error(`Problem with request: ${err.message}`));
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
 // console.dir(process)
 // console.dir(process.argv)
-const user = process.argv.slice(2)
-user.forEach(getProfile)
+const user = process.argv.slice(2);
+user.forEach(getProfile);
