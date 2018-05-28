4fb9cff42d485a5239a5b4ad12a27f66c5433ced
Ryan White <mrwhiterk@googlemail.com>
2018-05-28 00:06:02 -0400

    added router.js

--- e3_ttree_node_site/app.js
+++ e3_ttree_node_site/app.js
@@ -1,20 +1,24 @@
+var router = require("./router.js");
 //Problem: We need a simple way to look at a user's badgecount and js points from a web browser
 //Solution: Use Node.js to perform the profile look ups and serve our template via HTTP
 
 //1. Create a web server
-//2. Handle HTTP route GET / and POST / i.e. Home
-    //if url === "/" && GET
-        //show search
-    //if url== "/" && POST
-        //redirect to /:username
+const http = require('http');
 
-//3. Handle HTTP route GET /:username i.e. /chalkers
-    //if url == "/...."
-        //get json from Treehouse
-            //on "end"
-                //show profile
-            //on "error"
-                //show error
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+  router.home(request, response);
+  router.user(request, response);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
 //4. Function that handles the reading of files and merge in values
     // read from file and get a string
         // merge values into string
+
