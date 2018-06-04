491fbd56b1ea0b3119f576c226bead9cda381a51
Ryan White <mrwhiterk@gmail.com>
2018-06-04 20:42:17 +0000

    finished

--- e3_ttree_node_site/app.js
+++ e3_ttree_node_site/app.js
@@ -5,8 +5,8 @@ var router = require("./router.js");
 //1. Create a web server
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = process.env.IP;
+const port = process.env.PORT;
 
 const server = http.createServer((request, response) => {
   router.home(request, response);
